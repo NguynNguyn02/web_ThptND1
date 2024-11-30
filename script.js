0852391754543714a52c0cd327bd746c005a7225
trancamtu02 <mantuetran@gmail.com>
2024-11-30 12:51:30 +0700

    update

--- script.js
+++ script.js
@@ -141,19 +141,4 @@ nextBtn.addEventListener("click", () => {
   }
 });
 
-// Hiển thị trang đầu tiên khi tải trang
-displayPage(currentPage);
-updatePagination();
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("/header.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("header-placeholder").innerHTML = data;
-    });
 
-  fetch("/footer.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("footer-placeholder").innerHTML = data;
-    });
-});
